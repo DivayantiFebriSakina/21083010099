e9d0dc33056108b2619c9b01c679af65202086e1
DivayantiFebriSakina <113032310+DivayantiFebriSakina@users.noreply.github.com>
2022-11-02 18:53:05 +0700

    Function1.sh

---
+++ Function1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Mendeklarasikan fungsi
+
+nama() {
+    echo "Siapa namamu?"
+    read nama
+}
+npm() {
+   echo "Sebutkan npmmu"
+   read npm
+   echo -e "Hai $nama dengan npm $npm, selamat datang \n di praktikum sistem operasi
+yang seru ini ya!"
+}
+
+#memanggil fungsi
+nama
+npm
